2f584e94859bba4c1811948518054d05499ecdb5
alexis-jacq <alexis.jacq@gmail.com>
2016-05-27 12:08:40 +0200

    constructor for ToM agents

--- src/mutualModelling/reasoning.py
+++ src/mutualModelling/reasoning.py
@@ -8,6 +8,31 @@ import matplotlib.pyplot as plt
 
 class Agent:
     """agent able of first and 2nd mutual modelling reasoning"""
-    def __init__(self):
-        self.me = model.Model()
+    def __init__(self,name,agents,percepts,actions):
+        # name : string, name of the agent
+        # agents : list of strings, itself + all other agents
+        # percepts : list of strings
+        # action : list of string
+
+        self.name = name
+        if name not in agents:
+            agents.append(name)
+
+        self.M = {} # Mutual Models constructed by the agent
+        self.Id = {} # identification between 1st order and 2nd order point
+                     # of view of a same agent : a.Id[b][x] = b viewed by x for a
+                     # a.Id[b][c] = c,b
+                     # a.M[c,b] := the model build by a of (the model of b by c)
+
+        for agent1 in agents:
+            self.M[agent1] = model.Model()
+            self.M[agent1].add_cells([percepts])
+            self.M[agent1].add_actions([actions])
+            for agent2 in agents:
+                self.M[agent1+','+agent2] = model.Model()
+                self.M[agent1+','+agent2].add_cells([percepts])
+                self.M[agent1+','+agent2].add_actions([actions])
+                self.Id.setdefault(agent2,{})
+                self.Id[agent2].setdefault(agent1,agent1+','+agent2)
+
 
