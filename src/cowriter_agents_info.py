820ccb8b3ba235fa427794e6845bb9acb5946636
alexis-jacq <alexis.jacq@gmail.com>
2016-07-07 11:08:27 +0200

    node that collects info about agents

---
+++ src/cowriter_agents_info.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+import sys
+import time
+import rospy
+import json
+from std_msgs.msg import String, Empty, Float64
+from mutualModelling.agent import Agent
+
+# this node collect all information about agents states/obs/actions:
+# and publish in one topic per agent (human, robot)
+#-------------------------------------------------------------------------
+pub_robot_action = rospy.Publisher('robot_action_topic', Float64, queue_size=1)
+pub_human_action = rospy.Publisher('human_action_topic', Float64, queue_size=1)
+pub_robot_target = rospy.Publisher('robot_target_topic', Float64, queue_size=1)
+pub_human_target = rospy.Publisher('human_target_topic', Float64, queue_size=1)
+pub_robot_obs = rospy.Publisher('robot_obs_topic', Float64, queue_size=1)
+pub_human_obs = rospy.Publisher('human_obs_topic', Float64, queue_size=1)
+
+
+def onChangeHumanTarget(msg):
+    current_human_target = str(msg.data).split("_/")[1]
+    target = String()
+    target.data = current_human_target
+    pub_human_target.publish(target)
+    # it also defines an action of the human:
+    human_action = "looks_"+current_human_target
+    action = String()
+    action.data = human_action
+    pub_human_action.publish(action)
+
+def onChangeHumanAction(msg):
+    pub_human_action.publish(msg)
+
+def onChangeRobotAction(msg):
+    pub_robot_action.publish(msg)
+    robot_action = str(msg.data)
+    if "looks" == robot_action.split("_")[0] and len(robot_action.split("_"))>0:
+        # then it also defines a new visual target of the robot:
+        current_robot_target = '_'.join(robot_action.split("_")[1:])
+        target = String()
+        target.data = current_human_target
+        pub_robot_target.publish(target)
+
+# TODO:
+# def onChangeRobotObs(msg):
+#
+# def onChangeHumanObs(msg):
+
+if __name__=='__main__':
+
+    rospy.init_node("cowriter_agent_info")
+
+    while(True):
+        # TODO: get this info from cowriter nodes
+        # instead of a tool_simu:
+        #
+        # # get current task:
+        # rospy.Subscriber("state_activity", String, onChangeTask)
+        #
+        # # get human activity action
+        # rospy.Suscriber("")
+        #
+        # get robot action:
+        #
+        # get human action:
+
+        # get current human target: (~attention_tracker)
+        rospy.Subscriber("actual_focus_of_attention", String, onChangeHumanTarget)
+
+        # get human action: (~ tool~simu)
+        rospy.Subscriber("human_action", String, onChangeHumanAction)
+
+        # get robot action: (~ tool_simu)
+        rospy.Subscriber("robot_action", String, onChangeRobotAction)
+
+        # TODO: same with obs (not from agents actions but from the world)
+
+        rospy.sleep(1.0)
+
+    rospy.spin()
--- src/cowriter_mutual_modelling.py
+++ src/cowriter_mutual_modelling.py
@@ -8,12 +8,12 @@ import json
 from std_msgs.msg import String, Empty, Float64
 from mutualModelling.agent import Agent
 
-# this node publishes the choice of action by the robot:
-#-------------------------------------------------------
+# this node udate models of agents (by the robot) and publishes the choice of action by the robot:
+#-------------------------------------------------------------------------------------------------
 pub_robot_action = rospy.Publisher('robot_action_topic', Float64, queue_size=1)
 
-# create a mutual modeller agent "robot" that also model an agent "human":
-#-------------------------------------------------------------------------
+# create a mutual modeller agent "robot" that also model an agent "human" in cowriter:
+#-------------------------------------------------------------------------------------
 robot_percepts = ["child_progress","child_head","justified_reward","justified_punish"]
 robot_actions = ["converge","diverge","exaggerate","look_tablet","look_child_head","look_out","look_tablet","point_tablet"]
 robot_rewards = [["justified_reward",1.,1.],["justified_punish",1.,1],["with_me",1.,1.],["with_me",-1.,-1.],["child_progress",1.,1.]]
@@ -24,10 +24,9 @@ robot = agent.Agent("robot",["robot","human"],robot_percepts,robot_actions,robot
 objects = {"experimentator","selection_tablet","tablet","robot_head","outside"}
 human_attention = ""
 
-def humanAttention(object):
-
-def onReceiveInfoHuman(msg):
-
-def onReceiveInfoRobot(msg):
+# what the human can perceive about robot actions given his point of attention:
+# TODO: dictionnary
+# what the robot is expected to perceive about human action given robot's attention:
+# TODO: dictionnary
 
 if __name__='name':
