1d222f3f1faba023e848886c8a38e1370b003d2f
alexis-jacq <alexis.jacq@gmail.com>
2016-12-05 18:21:48 +0000

    does not need mtTkinter

--- setup.py
+++ setup.py
@@ -25,18 +25,3 @@ setup(name='nextChoice',
       packages=['nextChoice'],
       data_files=[('share/doc/nextChoice', ['AUTHORS', 'LICENSE', 'README.md'])]
       )
-setup(name='mtTkinter',
-      version='0.0',
-      license='ISC',
-      description='''This module modifies the original Tkinter module in memory, making all
-functionality thread-safe. It does this by wrapping the Tk class' tk
-instance with an object that diverts calls through an event queue when
-the call is issued from a thread other than the thread in which the Tk
-instance was created. The events are processed in the creation thread
-via an 'after' event.''',
-      author='Allen B. Taylor',
-      author_email='a.b.taylor@gmail.com',
-      package_dir = {'': 'src'},
-      packages=['mtTkinter'],
-      data_files=[('share/doc/mtTkinter', ['AUTHORS', 'LICENSE', 'README.md'])]
-      )
